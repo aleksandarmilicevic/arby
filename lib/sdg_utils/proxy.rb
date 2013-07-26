ccb3e3d93b59c6fae3dce04308a855459f3cbdab
Aleksandar Milicevic <aleks@csail.mit.edu>
2013-07-26 18:43:14 -0400

    3 different implementations of method instrumentation and a unit test

--- lib/sdg_utils/proxy.rb
+++ lib/sdg_utils/proxy.rb
@@ -40,42 +40,133 @@ module SDGUtils
     end
   end
 
-  class MethodInstrumenter
-    def self.around_object_methods(obj, name_pattern=/.*/, &block)
-      self.around_instance_methods(class << obj; self end, name_pattern, &block)
+  module MethodInstrumenter
+    extend self
+
+    def self.expand(method, default_impl)
+      %w(lambda send direct_call).each do |impl|
+      self.class_eval <<-RUBY, __FILE__, __LINE__+1
+def #{method}_#{impl}(obj, name_pattern=/.*/, cb_obj=nil, &block)
+  __#{method}(__#{impl}_opts, obj, name_pattern, cb_obj, &block)
+end
+RUBY
+      end
+      alias_method method.to_sym, "#{method}_#{default_impl}".to_sym
     end
 
-    def self.around_instance_methods(cls, name_pattern=/.*/, &block)
+    expand :around, :lambda
+    expand :before, :lambda
+    expand :after, :lambda
+
+    private
+
+    def __save_cb(bndg)
+      nil
+    end
+
+    def __around(opts, obj, name_pattern=/.*/, cb_obj=nil, &block)
+      alias_sym_proc     = opts[:alias_sym] || lambda{|bndg| 
+        m, rand = bndg.eval '[m, rand]'
+        "#{m}__#{rand}__".to_sym
+      }
+      define_method_proc = opts[:define_method] 
+      pre_proc           = opts[:pre] || lambda{|bndg|}
+      post_proc          = opts[:post] || lambda{|bndg|}
+      fail "no define_method_proc given" unless define_method_proc
+
+      store = {}
+      cls = class << obj; self end
+      cb = cb_obj || block
+      rand = SDGUtils::Random.salted_timestamp
+      fail "no callback given" unless cb
+
       mthds = cls.instance_methods.reject{|m|
         /(^__|^send$|^object_id$)/ === m
       }.grep(name_pattern)
+      
+      pre_proc.call(binding)
       mthds.each{|m|
-        alias_sym = "__#{Time.now.utc.strftime("%s_%L")}__#{m}".to_sym
+        alias_sym = alias_sym_proc.call(binding)
         cls.send :alias_method, alias_sym, m
-        cls.send :define_method, m, lambda{|*a, &b|
-          block.call(m, a, b) {
-            self.send alias_sym, *a, &b
+        define_method_proc.call(binding)
+      }
+      post_proc.call(binding)
+    end
+
+    def __before(opts, obj, name_pattern=/.*/, cb_obj=nil, &before_cb)
+      cb = cb_obj || before_cb
+      fail "no callback given" unless cb
+      __around(opts, obj, name_pattern) do |name, args, block, &yield_cb|
+        cb.call(name, args, block)
+        yield_cb.call
+      end
+    end
+
+    def __after(opts, obj, name_pattern=/.*/, cb_obj=nil, &after_cb)
+      cb = cb_obj || after_cb
+      fail "no callback given" unless cb
+      __around(opts, obj, name_pattern) do |name, args, block, &yield_cb|
+        ans = yield_cb.call
+        cb.call(name, args, block, ans)
+        ans
+      end
+    end
+
+    def __lambda_opts
+      {
+        :define_method => lambda{|bndg|
+          cls, rand, cb, m, alias_sym = bndg.eval "[cls, rand, cb, m, alias_sym]"
+          cls.send :define_method, m, lambda{|*a, &b|
+            cb.call(m, a, b) {
+              self.send alias_sym, *a, &b
+            }
           }
         }
       }
     end
 
-#     def self.around_instance_methods2(cls, name_pattern=/.*/, &block)
-#       mthds = cls.instance_methods.reject{|m|
-#         /(^__|^send$|^object_id$)/ === m
-#       }.grep(name_pattern)
-#       mthds.each{|m|
-#         rand = SDGUtils::Random.salted_timestamp
-#         alias_sym = "#{m}__#{rand}__".to_sym
+    def __send_opts
+      {
+        :pre => lambda{|bndg|
+          obj, cls, cb, rand = bndg.eval '[obj, cls, cb, rand]'
+          cb_var = "cb__#{rand}"
+          obj.instance_variable_set "@#{cb_var}", cb
+          cls.send :attr_reader, cb_var.to_sym
+          bndg.eval "store[:cb_var] = #{cb_var.inspect}"
+        }, 
+        :define_method => lambda{|bndg|
+          cls, cb_var, m, alias_sym = bndg.eval "[cls, store[:cb_var], m, alias_sym]"
+          cls.class_eval <<-RUBY, __FILE__, __LINE__+1
+def #{m}(*args, &block)
+  #{cb_var}.call(#{m.inspect}, args, block) {
+    self.send #{alias_sym.inspect}, *args, &block
+  }
+end
+RUBY
+        }
+      }
+    end
 
-#         cls.send :alias_method, alias_sym, m
-#         cls.class_eval <<-RUBY, __FILE__, __LINE__+1
-# def #{m}(*args, &block)
-#   self.send #{alias_sym.inspect}, *args, &block
-# end
-# RUBY
-#       }
-#     end
+    def __direct_call_opts
+      {
+        :pre => __send_opts[:pre],
+        :alias_sym => lambda{|bndg|
+          m, rand = bndg.eval '[m, rand]'
+          "__#{m.hash.abs}__#{rand}__".to_sym
+        },
+        :define_method => lambda{|bndg|
+          cls, cb_var, m, alias_sym = bndg.eval "[cls, store[:cb_var], m, alias_sym]"
+          cls.class_eval <<-RUBY, __FILE__, __LINE__+1
+def #{m}(*args, &block)
+  @#{cb_var}.call(#{m.inspect}, args, block) {
+    #{alias_sym}(*args, &block)
+  }
+end
+RUBY
+        }
+      }
+    end
+
   end
 
 end
--- test/unit/sdg_utils/method_instrumenter_test.rb
+++ test/unit/sdg_utils/method_instrumenter_test.rb
@@ -4,23 +4,99 @@ require 'sdg_utils/proxy.rb'
 module SDGUtils
 
   class MethodInstrumenterTest < Test::Unit::TestCase
-    # def test0
-    #   a = [1, 2, 3]
-    #   SDGUtils::MethodInstrumenter.before_object_methods(a){|name|
-    #     puts "called: #{name}"
-    #   }
-    #   a.size
-    # end
+    include SDGUtils::MethodInstrumenter
 
-    def test1
-      a = [1, 2, 3]
-      SDGUtils::MethodInstrumenter.around_object_methods(a){|name,args,block,&cont|
-        puts "called: #{name}"
-        cont.call
-      }
-      puts a.size
+    def instr(n, instr_method, cb)
+      x = (0...n).to_a
+      c = class << x; self end
+      send instr_method, x, /.*/, cb
+      #send instr_method, x, /^[a-z_A-Z0-9]*$/, cb
+      #send instr_method, x, /^size$/, cb
+      {:method => instr_method, :obj => x, :cls => c, :cb => cb}
     end
 
-  end
+    def test_around
+      cls = Class.new do
+        def calls() @calls ||= 0 end
+        def call(name, args, block, &yield_cb) 
+          @calls = calls + 1
+          yield_cb.call
+        end
+      end
+      do_bench :around, cls
+    end
 
+    def test_before
+      cls = Class.new do
+        def calls() @calls ||= 0 end
+        def call(name, args, block) 
+          @calls = calls + 1
+        end
+      end
+      do_bench :before, cls
+    end
+
+    def test_after
+      cls = Class.new do
+        def calls() @calls ||= 0 end
+        def call(name, args, block, value) 
+          @calls = calls + 1
+        end
+      end
+      do_bench :after, cls
+    end
+
+    def do_bench(instr_method, cb_cls)
+      n = 1000
+
+      arr = %w(lambda send direct_call).map do |impl|
+        instr n, "#{instr_method}_#{impl}".to_sym, cb_cls.new
+      end
+      
+      bench = Proc.new do |m, &action|
+        Benchmark.bm(30) do |reporter|
+          arr.each do |hash|
+            reporter.report("#{hash[:method]}: ") {m.times{action.call(hash[:obj])}}
+          end
+        end
+      end
+      
+      expected = 0
+
+      m = 100
+      bench.call(m) {|x| 
+        ans = x.reduce(0){|a,e| a+e}
+        assert_equal n*(n-1)/2, ans
+        ans
+      }
+
+      expected += m*2
+      arr.each do |hash|
+        assert_equal expected, hash[:cb].calls
+      end
+
+      m = 10000
+      bench.call(m) { |x|
+        ans = x.size
+        assert_equal n, ans
+        ans
+      }
+      
+      expected += m
+      arr.each do |hash|
+        assert_equal expected, hash[:cb].calls
+      end
+      
+      bench.call(m) {|x| x << 1}
+      
+      expected += m
+      arr.each do |hash|
+        assert_equal expected, hash[:cb].calls
+      end
+      arr.each do |hash|
+        assert_equal n+m, hash[:obj].size
+      end    
+    end
+    
+  end
 end
