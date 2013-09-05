a6fff48b9f5e9a6558152096881fc6f4c4ea595c
Aleksandar Milicevic <aleks@csail.mit.edu>
2013-09-05 10:21:09 -0400

    added a unit test for nested classes

--- lib/sdg_utils/delegator.rb
+++ lib/sdg_utils/delegator.rb
@@ -4,14 +4,34 @@ module SDGUtils
     # #TODO def respond_to?
 
     def method_missing(name, *args, &block)
-      return unless @target
-      handler = ::Proc.new do |*a, &b|
-        obj = @target
-        obj = @target.call() if ::Proc === @target && @target.arity == 0
-        obj.send(name, *a, &b)
+      return super unless @target
+      begin 
+        # let the super method_missing run first (if defined)
+        super
+      rescue ::NameError
+        handler = ::Proc.new do |*a, &b|
+          obj = @target
+          obj = @target.call() if ::Proc === @target && @target.arity == 0
+          obj.send(name, *a, &b)
+        end
+        (class << self; self end).send :define_method, name, handler
+        handler.call(*args, &block)
       end
-      (class << self; self end).send :define_method, name, handler
-      handler.call(*args, &block)
+    end
+  end
+
+  module MNested
+    include MDelegator
+
+    def self.included(base)
+      base.class_eval <<-RUBY, __FILE__, __LINE__+1
+        class << self
+          private
+          def new(*a, &b) super end
+        end
+        private
+        def initialize(parent) @target = parent end
+      RUBY
     end
   end
 
