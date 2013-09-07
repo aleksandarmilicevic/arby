b12f65c853a2bd23665753e88169e869775a9303
Aleksandar Milicevic <aleks@csail.mit.edu>
2013-09-07 16:12:48 -0400

    in TypeChecker, added an option to pass an error message

--- lib/alloy/ast/type_checker.rb
+++ lib/alloy/ast/type_checker.rb
@@ -26,15 +26,17 @@ module Alloy
         actual <= expected #TODO: incomplete
       end
 
-      def check_sig_class(cls, supercls=Alloy::Ast::ASig)
-        raise_not_sig = proc{raise TypeError, "#{cls} is not a #{supercls} class"}
+      def check_sig_class(cls, supercls=Alloy::Ast::ASig, msg="")
+        msg = "#{msg}\n" unless msg.to_s.empty?
+        raise_not_sig = proc{raise TypeError, "#{msg}#{cls} is not a #{supercls} class"}
         raise_not_sig[] unless Class === cls
         raise_not_sig[] unless cls < supercls
       end
 
-      def check_alloy_module(mod)
+      def check_alloy_module(mod, msg="")
+        msg = "#{msg}\n" unless msg.to_s.empty?
         raise_not_mod = proc{
-          raise TypeError, "#{mod} is not a ruby module used as Alloy model"
+          raise TypeError, "#{msg}#{mod} is not a ruby module used as Alloy model"
         }
         raise_not_mod[] unless Module === mod
         raise_not_mod[] unless mod.respond_to? :meta
