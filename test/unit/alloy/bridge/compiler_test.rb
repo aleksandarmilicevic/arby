4785b92717969906d0c4d034e581942ffdfe921c
Aleksandar Milicevic <aleks@csail.mit.edu>
2013-11-18 09:13:21 -0500

    added a test for 'all_fields'

--- test/unit/alloy/bridge/compiler_test.rb
+++ test/unit/alloy/bridge/compiler_test.rb
@@ -5,23 +5,44 @@ module Alloy
   module Bridge
 
     class CompilerTest < Test::Unit::TestCase
-      def test_compiler
-        model = "sig A { f: set A}\n\n run { #f > 1 && #A = 2 } for 4"
-        compiler = Compiler.new
-        a4world = compiler.compute_world(model)
-        a4sol = compiler.execute_command(a4world)
-        a4fields = compiler.all_fields(a4world)
-        a4atoms = compiler.all_atoms(a4sol)
-        assert_equal 2, a4atoms.size
+      include SDGUtils::Testing::SmartSetup
+      include SDGUtils::Testing::Assertions
+
+      @@model = """
+sig A { 
+  f: set A,
+  g: Int
+}
+
+run { 
+  #f > 1
+} for 4 but exactly 3 A
+"""
+
+      def setup_class
+        @@compiler = Compiler.new
+        @@a4world = @@compiler.compute_world(@@model)
+        @@a4sol = @@compiler.execute_command(@@a4world)
+      end
+
+      def test_all_atoms
+        a4atoms = @@compiler.all_atoms(@@a4sol)
+        assert_equal 3, a4atoms.size
         assert_equal "A$0", a4atoms.get(0).toString
         assert_equal "A$1", a4atoms.get(1).toString
-        # atoms = compiler.list_of_atoms_from_fields(fields, sol)
-        # assert_equal atoms.size, 2
-        # assert_equal atoms[0].size, 2
-        # assert_equal atoms[1].size, 2
-        # assert_equal atoms[0], ["A$1", "A$1"]
-        # assert_equal atoms[1], ["A$1", "A$0"]
+        assert_equal "A$2", a4atoms.get(2).toString
       end
+
+      def test_all_fields
+        a4fields = @@compiler.all_fields(@@a4world)
+        
+        field_names = a4fields.map &:label
+        assert_set_equal ["f", "g"], field_names
+        
+        field_owners = a4fields.map(&:sig).map(&:label)
+        assert_seq_equal ["this/A", "this/A"], field_owners
+      end
+
     end
   end
 end
