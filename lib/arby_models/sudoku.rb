7e3223e69f3db5c72cbb5e0a02f598792f083b9c
Aleksandar Milicevic <aleks@csail.mit.edu>
2013-12-10 18:34:02 -0500

    added an alternative formulation of the 'solved' predicate which combines symbolic and concrete execution

--- lib/arby_models/sudoku.rb
+++ lib/arby_models/sudoku.rb
@@ -1,4 +1,5 @@
 require 'alloy/alloy_dsl'
+require 'alloy/ast/expr_builder'
 
 module ArbyModels
   extend Alloy::Dsl
@@ -10,7 +11,25 @@ module ArbyModels
 
     pred solved[s: Sudoku] {
       s.grid[0..8][Int] == (1..9) and
-      s.grid[Int][0..8] == (1..9)
+      s.grid[Int][0..8] == (1..9) and
+      s.grid[0..2][0..2] == (1..9) and
+      s.grid[0..2][3..5] == (1..9) and
+      s.grid[0..2][6..8] == (1..9) and
+      s.grid[3..5][0..2] == (1..9) and
+      s.grid[3..5][3..5] == (1..9) and
+      s.grid[3..5][6..8] == (1..9) and
+      s.grid[6..8][0..2] == (1..9) and
+      s.grid[6..8][3..5] == (1..9) and
+      s.grid[6..8][6..8] == (1..9)
+    }
+
+    pred solved2[s: Sudoku] {
+      sq = [(0..2), (3..5), (6..8)]
+      exprs = sq.product(sq).map{|r, c| s.grid[r][c] == (1..9)}
+
+      s.grid[0..8][Int] == (1..9) and
+      s.grid[Int][0..8] == (1..9) and
+      conj(*exprs)
     }
   end
 
