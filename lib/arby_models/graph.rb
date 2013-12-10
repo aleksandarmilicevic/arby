b83077e0086de147253718c6a24ec38cb8638cc0
Aleksandar Milicevic <aleks@csail.mit.edu>
2013-12-10 10:52:40 -0500

    added a graph model skeleton

---
+++ lib/arby_models/graph.rb
@@ -0,0 +1,24 @@
+require 'alloy/alloy_dsl'
+
+module ArbyModels
+  extend Alloy::Dsl
+
+  alloy_model :Graph do
+
+    sig Node [
+      label: Int
+    ]
+
+    sig Edge [
+      src: Node, 
+      dst: Node,
+      cost: Int
+    ]
+
+    sig Graph [
+      nodes: (set Node),
+      edges: (set Edge)
+    ]
+
+  end
+end
