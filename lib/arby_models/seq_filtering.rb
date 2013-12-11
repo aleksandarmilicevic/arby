7811198ae4e6d4dbc543e659b182565371e845a9
Aleksandar Milicevic <aleks@csail.mit.edu>
2013-12-11 15:24:46 -0500

    sequence filtering example

---
+++ lib/arby_models/seq_filtering.rb
@@ -0,0 +1,36 @@
+require 'alloy/alloy_dsl'
+
+Alloy.conf.sym_exe.convert_missing_fields_to_joins = true
+
+module ArbyModels
+  extend Alloy::Dsl
+
+  alloy_model :SeqFiltering do
+    sig A [
+      x: Int
+    ]
+
+    fun prevOccurrences[s: (seq A), idx: Int][set Int] {
+      s.indsOf(s[idx]).select{|i| i < idx}
+    }
+
+    pred filter[s: (seq A), ans: (seq A)] {
+      filtered = s.elems.select{|a| a.x < 3}
+      ans.elems == filtered and
+      all(a: filtered) { ans.a.size == s.a.size } and
+      all(i1: s.inds, i2: s.inds) {
+        if i2 > i1 && filtered.contains?(s[i1] + s[i2])
+          some(ii1: ans.inds, ii2: ans.inds) {
+            ii2 > ii1 and
+            ans[ii1] == s[i1] and
+            ans[ii2] == s[i2] and
+            prevOccurrences(s, i1) == prevOccurrences(ans, ii1) and
+            prevOccurrences(s, i2) == prevOccurrences(ans, ii2)
+          }
+        end
+      }
+    }
+  end
+
+
+end
---
+++ test/unit/alloy/model/seq_filtering_test.rb
@@ -0,0 +1,22 @@
+require 'my_test_helper'
+require 'arby_models/seq_filtering'
+
+class SeqFilteringTest < Test::Unit::TestCase
+  include SDGUtils::Testing::SmartSetup
+  include SDGUtils::Testing::Assertions
+
+  include ArbyModels::SeqFiltering
+
+  def setup_class
+    Alloy.reset
+    Alloy.meta.restrict_to(ArbyModels::SeqFiltering)
+    Alloy.initializer.init_all_no_freeze
+
+    @@als_model = Alloy.meta.to_als
+  end
+
+  def test1
+    puts @@als_model
+  end
+
+end
