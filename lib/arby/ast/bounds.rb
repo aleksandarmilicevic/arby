8d3c44b4a97f186d5b4a32e7a6ce7725ae08d709
Aleksandar Milicevic <aleks@csail.mit.edu>
2013-12-25 15:16:00 -0500

    Refactored Bounds and added more methods for defining bounds.

--- lib/arby/ast/bounds.rb
+++ lib/arby/ast/bounds.rb
@@ -6,7 +6,7 @@ require 'arby/ast/type_checker'
 module Arby
   module Ast
 
-    # Rel: [Class(Arby::Ast::Sig), Arby::Ast::Field]
+    # Rel: [Class(Arby::Ast::Sig), Arby::Ast::Field, Arby::Ast::TypeConsts::Int]
     #
     # @attr sig_lowers, sig_uppers [Hash(Rel, Arby::Ast::TupleSet)]
     # @attr fld_lowers, fld_uppers [Hash(Rel, Arby::Ast::TupleSet)]
@@ -17,18 +17,14 @@ module Arby
         @uppers = {}
       end
 
-      def bound(what, lower, upper)
-        TypeChecker.check_arity(what, lower)
-        TypeChecker.check_arity(what, upper)
-        check_field_or_sig(what)
-        @lowers[what] = lower.dup
-        @uppers[what] = upper.dup
+      def bound(what, lower, upper=nil)
+        set_bound(@lowers, what, lower)
+        set_bound(@uppers, what, upper || lower)
         self
       end
 
-      def bound_exactly(what, tuple_set)
-        bound(what, tuple_set, tuple_set)
-      end
+      def bound_exactly(what, tuple_set) bound(what, tuple_set, nil) end
+      def bound_int(lower)               bound_exactly(TypeConsts::Int, lower) end
 
       def add_lower(what, tuple_set)
         add_bound(@lowers, what, tuple_set)
@@ -39,24 +35,47 @@ module Arby
         add_bound(@uppers, what, tuple_set)
       end
 
+      def set_lower(what, tuple_set) set_bound(@lowers, what, tuple_set) end
+      def set_upper(what, tuple_set) set_bound(@uppers, what, tuple_set) end
+
       def get_lower(what) @lowers[what] end #dup ???
       def get_upper(what) @uppers[what] end #dup ???
 
       private
 
-      def add_bound(where, what, tuple_set)
-        check_field_or_sig(what)
-        bnd = get_or_new(where, what)
-        bnd.union!(tuple_set)
+      def set_bound(where, what, tuple_set)
+        tuple_set = TupleSet.wrap(tuple_set)
+        type = check_bound(what, tuple_set)
+        ts = get_or_new(where, what, type)
+        ts.clear!
+        ts.union!(tuple_set)
         self
       end
 
-      def get_or_new(col, what)
-        col[what] ||= TupleSet.wrap([], what)
+      def add_bound(where, what, tuple_set)
+        tuple_set = TupleSet.wrap(tuple_set)
+        type = check_bound(what, tuple_set)
+        ts = get_or_new(where, what, type)
+        ts.union!(tuple_set)
+        self
       end
 
-      def check_field_or_sig(what)
-        unless what.is_a?(Field) || TypeChecker.check_sig_class(what)
+      def check_bound(what, tuple_set)
+        type = check_boundable(what)
+        TypeChecker.check_subtype!(type, tuple_set._type)
+        type
+      end
+
+      def get_or_new(col, what, type)
+        col[what] ||= TupleSet.wrap([], type)
+      end
+
+      def check_boundable(what)
+        case
+        when Field === what                    then what.full_type
+        when TypeChecker.check_sig_class(what) then what.to_atype
+        when TypeConsts::Int == what           then TypeConsts::Int
+        else
           raise TypeError, "only Field instances or Sig class can be bound; got #{what}"
         end
       end
