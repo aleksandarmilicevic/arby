d0ed23f3c539346601639bddd5c5f225afef0143
Aleksandar Milicevic <aleks@csail.mit.edu>
2014-01-05 10:25:29 -0500

    removed unnecessary code

--- lib/arby/dsl/fun_instrumenter.rb
+++ lib/arby/dsl/fun_instrumenter.rb
@@ -15,30 +15,29 @@ module Arby
         return ["", ""] unless ast
         orig_src = read_src(ast)
         instr_src = reprint(ast) do |node, parent, anno|
-          new_src =
-            case node.type
-            when :if
-              cond_src = compute_src(node.children[0], anno)
-              then_src = compute_src(node.children[1], anno)
-              else_src = compute_src(node.children[2], anno)
-              if else_src.empty?
-                "Arby::Ast::Expr::BinaryExpr.implies(" +
-                  "#{cond_src}, proc{#{then_src}}) "
-              else
-                "Arby::Ast::Expr::ITEExpr.new(" +
-                  "#{cond_src}, " +
-                  "proc{#{then_src}}, " +
-                  "proc{#{else_src}})"
-              end
-            when :and, :or
-              lhs_src = compute_src(node.children[0], anno)
-              rhs_src = compute_src(node.children[1], anno)
-              "Arby::Ast::Expr::BinaryExpr.#{node.type}(" +
-                "proc{#{lhs_src}}, " +
-                "proc{#{rhs_src}})"
+          case node.type
+          when :if then
+            cond_src = compute_src(node.children[0], anno)
+            then_src = compute_src(node.children[1], anno)
+            else_src = compute_src(node.children[2], anno)
+            if else_src.empty?
+              "Arby::Ast::Expr::BinaryExpr.implies(" +
+                "#{cond_src}, proc{#{then_src}}) "
             else
-              nil
+              "Arby::Ast::Expr::ITEExpr.new(" +
+                "#{cond_src}, " +
+                "proc{#{then_src}}, " +
+                "proc{#{else_src}})"
             end
+          when :and, :or then
+            lhs_src = compute_src(node.children[0], anno)
+            rhs_src = compute_src(node.children[1], anno)
+            "Arby::Ast::Expr::BinaryExpr.#{node.type}(" +
+              "proc{#{lhs_src}}, " +
+              "proc{#{rhs_src}})"
+          else
+            nil
+          end
         end
         [orig_src, instr_src]
       end
