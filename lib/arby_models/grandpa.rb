5304b84c53cfec47cb82f045408b2414d5450869
Aleksandar Milicevic <aleks@csail.mit.edu>
2014-01-05 12:43:40 -0500

    grandpa example

---
+++ lib/arby_models/grandpa.rb
@@ -0,0 +1,53 @@
+require 'arby/arby_dsl'
+
+Arby.conf.sym_exe.convert_missing_fields_to_joins = true
+
+module ArbyModels
+  extend Arby::Dsl
+
+  alloy :Grandpa do
+    abstract sig Person [
+      father: (lone Man),
+      mother: (lone Woman)
+    ] {
+      not self.in? self.^(Person.father + Person.mother)
+    }
+
+    sig Man extends Person [
+      wife: (lone Woman)
+    ]
+
+    sig Woman extends Person [
+      husband: (lone Man)
+    ]
+
+    fact terminology { wife == ~husband }
+
+    fact socialConvention {
+      no wife & (mother+father).rclosure.mother and
+      no husband & (mother+father).rclosure.father
+    }
+
+    fun grandpas[p: Person][set Person] {
+      let(parent: mother + father + father.wife + mother.husband) {
+        p.parent.parent & Man
+      }
+    }
+
+    pred ownGrandpa[m: Man] {
+      m.in? grandpas(m)
+    }
+
+    run :ownGrandpa, "for 4 Person"
+
+    # fact socialConvention2 {
+    #   no wife & mother.*(~(mother+father)) and
+    #   no husband & father.*(~(mother+father))
+    # }
+
+    # fun grandpas2[p: Person][set Person] {
+    #   parent = mother + father + father.wife + mother.husband
+    #   p.join(parent).join(parent) & Man
+    # }
+  end
+end
---
+++ test/unit/arby/model/grandpa_test.rb
@@ -0,0 +1,25 @@
+require 'my_test_helper'
+require 'arby_models/grandpa'
+
+class GraphTest < Test::Unit::TestCase
+  include SDGUtils::Testing::SmartSetup
+  include SDGUtils::Testing::Assertions
+  include Arby::Bridge
+
+  include ArbyModels::Grandpa
+
+  def setup_class
+    Arby.reset
+    Arby.meta.restrict_to(ArbyModels::Grandpa)
+  end
+
+  def test_als
+    puts Arby.meta.to_als
+  end
+
+  def test_instance
+    sol = ArbyModels::Grandpa.execute_command
+    assert sol.satisfiable?
+  end
+
+end
