125b38bc50016c260169ce670c9a3a53f4e3998b
Aleksandar Milicevic <aleks@csail.mit.edu>
2014-01-29 09:05:15 -0500

    minor

--- lib/arby_models/prisoners_hats.rb
+++ lib/arby_models/prisoners_hats.rb
@@ -5,27 +5,33 @@ module ArbyModels
 
   alloy :PrisonersHats do
 
-    abstract sig Color
-    one sig Red extends Color
-    one sig Blue extends Color
-
-    procedure colors_fact[rel] {
-      rel.(Red).size == 2 && rel.(Blue).size == 2
-    }
+    # abstract sig Color
+    # one sig Red extends Color
+    # one sig Blue extends Color
+    enum Color(Red, Blue)
 
     ordered sig Prisoner [
       hatColor: Color,
       deduced1: Prisoner ** (one Color),
       deduced2: Prisoner ** (one Color)
     ] {
-      colors_fact(this.deduced1) and
-      colors_fact(this.deduced2) and
-      (this.sees.< Prisoner::hatColor).in? this.deduced1 and
-      (this.sees.< Prisoner::hatColor).in? this.deduced2
+      # deduction_rule(this, this.deduced1) and
+      # deduction_rule(this, this.deduced2)
+      # colors_fact(this.deduced1) and
+      # colors_fact(this.deduced2) and
+      # (this.sees.< Prisoner::hatColor).in? this.deduced1 and
+      # (this.sees.< Prisoner::hatColor).in? this.deduced2
       # all(p: this.sees){
       #   this.deduced1[p] == p.hatColor &&
       #   this.deduced2[p] == p.hatColor
       # }
+    }
+
+    pred colors_fact[rel: Prisoner ** Color] {
+      rel.(Red).size == 2 && rel.(Blue).size == 2
+    }
+    pred deduction_rule[p: Prisoner, rel: Prisoner ** Color] {
+      colors_fact(rel) and (sees(p).< hatColor).in? rel
     }
 
     fun sees[p: Prisoner][set Prisoner] {
@@ -33,7 +39,8 @@ module ArbyModels
     }
 
     fact hatColors {
-      colors_fact(hatColor)
+      colors_fact(hatColor) and
+      all(p: Prisoner) { deduction_rule(p, p.deduced1) && deduction_rule(p, p.deduced2) }
     }
 
     pred allAmbig {
