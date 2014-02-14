89bdfd4d4ca6cd9a887585ce0a30240ef5c4314e
Aleksandar Milicevic <aleks@csail.mit.edu>
2014-02-14 10:12:21 -0500

    moved the graph example

--- lib/arby_models/graph.rb
+++ lib/arby_models/graph.rb
@@ -1,37 +1,46 @@
 require 'arby/arby_dsl'
+require 'arby/ast/bounds'
 
 module ArbyModels
   extend Arby::Dsl
 
-  alloy_model :GraphModel do
+  alloy :GraphModel do
+    sig Node [val: (lone Int)]
+    sig Edge [src, dst: (one Node)] {src != dst}
+    sig Graph[nodes:(set Node), edges:(set Edge)]
 
-    sig Node [
-      val: (lone Int)
-    ]
-
-    sig Edge [
-      src, dst: Node,
-      cost:     (lone Int)
-    ]
-
-    sig Graph [
-      nodes: (set Node),
-      edges: (set Edge)
-    ]
-
-    pred hamiltonian[g: Graph, ans: (seq Node)] {
-      ans[Int] == g.nodes and
-      all(i: 0...ans.size-1) {
-        some(e: g.edges) { e.src == ans[i] && e.dst == ans[i+1] }
-      }
+    pred hampath[g: Graph, path: (seq Node)] {
+      g.nodes.size == 2 and some g.edges and
+      path[Int] == g.nodes and
+      path.size == g.nodes.size and
+      all(i: 0...path.size-1) |
+      some(e: g.edges) {
+        e.src == path[i] &&
+        e.dst == path[i+1] }
     }
-
-    pred hamiltonian2[g: Graph, ans: (seq Edge)] {
-      ans[Int].in? g.edges and
-      ans[Int].src + ans[Int].dst == g.nodes and
-      ans[Int].size == g.nodes.size - 1 and
-      all(i: 0...ans.size-1) { ans[i].dst == ans[i+1].src }
+    assertion reach {
+      all(g: Graph, path: (seq Node)) {
+        g.nodes.in? path[0].*((~src).dst) if hampath(g, path)}
     }
-
+    assertion uniq {
+      all(g: Graph, path: (seq Node)) |
+        if hampath(g, path)
+          all(n: g.nodes) { path.(n).size == 1 }
+        end
+    }
+    run :hampath, 5, Graph=>exactly(1), Node=>3
+    check :reach, 5, Graph=>exactly(1), Node=>3
+    check :uniq,  5, Graph=>exactly(1), Node=>3
   end
+
+  class GraphModel::Graph
+    def find_hampath
+      bnds = Arby::Ast::Bounds.from_atoms(self)
+      sol = GraphModel.solve :hampath, bnds
+      if sol.satisfiable?
+      then sol["$hampath_path"].project(1)
+      else nil end
+    end
+  end
+
 end
--- test/unit/arby/model/graph_test.rb
+++ test/unit/arby/model/graph_test.rb
@@ -1,17 +1,13 @@
 require 'my_test_helper'
-require 'arby_models/graph'
-require 'arby/helpers/test/dsl_helpers'
-require 'arby/initializer.rb'
-require 'arby/bridge/compiler'
-require 'arby/bridge/solution'
+require 'arby_models/abz14/graph'
 
 class GraphTest < Test::Unit::TestCase
-  include Arby::Helpers::Test::DslHelpers
   include SDGUtils::Testing::SmartSetup
   include SDGUtils::Testing::Assertions
   include Arby::Bridge
 
-  include ArbyModels::GraphModel
+  GraphModel = ArbyModels::GraphModel
+  include GraphModel
 
   def setup_class
     Arby.reset
@@ -19,32 +15,51 @@ class GraphTest < Test::Unit::TestCase
   end
 
   def test_als
-    puts Arby.meta.to_als
+    puts GraphModel.meta.to_als
+    assert GraphModel.compile
   end
 
-  def test_ham
-    n = 5
-    vals = Array(0...n)
-    nodes = vals.map{|i| Node.new :val => i}
+  def test_exe_spec
+    n1, n2 = Node.new, Node.new
+    e = Edge.new src: n1, dst: n2
+    g = Graph.new nodes: [n1, n2], edges: [e]
+    hp = g.find_hampath # => [n1, n2]
+    assert_equal [n1, n2], hp.unwrap
 
-    assert_equal vals, nodes.map(&:val)
+    hp = g.hampath.project(1) # => [n1, n2]
+    assert_equal [n1, n2], hp.unwrap
+  end
 
-    edges = (0...n-1).map{|i| Edge.new :src => nodes[i], :dst => nodes[i+1]}
-    g = Graph.new :nodes => nodes.shuffle, :edges => edges
-
-    ham_path = g.hamiltonian
-    assert ham_path, "expected to find hamiltonian path"
-    assert_equal Arby::Ast::TupleSet.wrap(vals.map{|i| [i]}), ham_path.val.project(1)
-
-    # the same thing manually
-    bnd = Arby::Ast::Instance.from_atoms(g).to_bounds
-    sol = ArbyModels::GraphModel.solve :hamiltonian, n, bnd
-    assert_equal vals, nodes.map(&:val)
+  def test_run_hampath
+    sol = GraphModel.run_hampath
     assert sol.satisfiable?
-    inst = sol.arby_instance
-    ham_path = inst["$hamiltonian_ans"]
+    assert graph = sol["$hampath_g"]
+    assert path  = sol["$hampath_path"]
+    puts graph.nodes
+    puts graph.edges
+    puts path.project(1)
+  end
 
-    assert_equal Arby::Ast::TupleSet.wrap(vals.map{|i| [i]}), ham_path.val.project(1)
+  def test_check_reach
+    sol = GraphModel.check_reach
+    assert !sol.satisfiable? # assertion holds
+  end
+
+  def test_check_uniq
+    sol = GraphModel.check_uniq
+    assert !sol.satisfiable? # assertion holds
+  end
+
+  def test_guided
+    sol = GraphModel.run_hampath
+    assert sol.satisfiable?
+    sol2 = sol.next { # "$hampath_path" != sol["$hampath_path"] &&
+      g = sol[Graph].first
+      Graph::nodes == sol[Graph::nodes] and
+      Graph::edges == sol[Graph::edges] and
+      (g.edges.< sol[Edge::dst]).in? Edge::dst and
+      (g.edges.< sol[Edge::src]).in? Edge::src
+    }
   end
 
 end
